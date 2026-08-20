a0d1e81d908ae7f1803cce8112485d43b718a835
bhargavidevops1 <bhargavibysani2001@gmail.com>
2026-08-21 03:05:46 +0530

    logs

--- logs.sh
+++ logs.sh
@@ -17,7 +17,7 @@ echo " $2 installation successful"
 fi
 }
 
-dnf list installed mysql -y &>> $logdetails 
+dnf list installed mysql &>> $logdetails 
 
 if [ $? -eq 0 ]; then
 echo "aleady installed"
